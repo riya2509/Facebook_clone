fa6ecee6121b667bfeafddd7274f751cbeef9d15
riya2509 <riyamars3@gmail.com>
2023-04-11 22:31:21 +0530

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmjd2wW4ObKn07m31gg-7h9qqkbH-ePOA",
+  authDomain: "facebook-clone-43d6c.firebaseapp.com",
+  projectId: "facebook-clone-43d6c",
+  storageBucket: "facebook-clone-43d6c.appspot.com",
+  messagingSenderId: "586245572434",
+  appId: "1:586245572434:web:34948797873f96a9c71c8b",
+  measurementId: "G-RSNT6K5PCV",
+};
---
+++ src/Header.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./App.css";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import FlagIcon from "@mui/icons-material/Flag";
+import {
+  SubscriptionsOutlined,
+  StorefrontOutlined,
+  SupervisedUserCircle,
+  Forum,
+  ExpandMore,
+} from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"
+          alt=""
+        />
+        <div className="header_input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header_center">
+        <div className="header_option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionsOutlined fontSize="large" />
+        </div>
+        <div className="header_option">
+          <StorefrontOutlined fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervisedUserCircle fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header_right">
+        <div className="header_info">
+          <Avatar />
+          <h4>Riya</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <Forum />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMore />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
