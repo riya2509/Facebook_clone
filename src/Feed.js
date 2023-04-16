312842edfe4f95f533139d5c7cf74cd65e58b84d
riya2509 <riyamars3@gmail.com>
2023-04-16 22:50:15 +0530

    working on the posts

--- src/Feed.js
+++ src/Feed.js
@@ -11,6 +11,19 @@ function Feed() {
       {/* StoryReel */}
       <MessageSender />
       {/* MessageSender */}
+      <Post
+        profilePic="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+        message="Wow! This works"
+        timestamp="This is a timestamp."
+        username="rrriyasat"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc23Vd2IVwcCmXusDUpSfwvGXFAreQ2bWf-A&usqp=CAU"
+      />
+      <Post
+        profilePic="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+        message="Wow! This works"
+        timestamp="This is a timestamp."
+        username="rrriyasat"
+      />
       <Post />
       {/* Post */}
     </div>
--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,26 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 import "./Post.css";
 
-function Post() {
-  return <div className="post"></div>;
+function Post({ profilePic, image, username, timestamp, message }) {
+  return (
+    <div className="post">
+      <div className="postTop">
+        <Avatar src={profilePic} className="post_avatar" />
+        <div className="post_topInfo">
+          <h3>{username}</h3>
+          <p>Timestamp...</p>
+        </div>
+      </div>
+      <div className="post_bottom">
+        <p>{message}</p>
+      </div>
+
+      <div className="post_image">
+        <img src={image} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Post;
